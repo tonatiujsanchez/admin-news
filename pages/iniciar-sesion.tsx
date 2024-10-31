1e27e51dcaa2505d6b136416e0a4c2ce2eab9593
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-10-31 17:47:26 -0600

    Refresh en la autenticación, añadido

--- pages/iniciar-sesion.tsx
+++ pages/iniciar-sesion.tsx
@@ -43,7 +43,8 @@ const IniciarSesionPage:NextPage = () => {
             return
         }
 
-        router.replace('/admin')
+        // router.replace('/admin')
+        router.reload()
     }
 
 
